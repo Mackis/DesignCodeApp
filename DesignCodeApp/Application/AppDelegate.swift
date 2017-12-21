754ebb825631a892d836d70bce7f32e26eaa0e07
Malcolm Kumwenda <malcolm.kumwenda@zapper.com>
2017-12-21 13:07:41 +0200

    initial commit

---
+++ DesignCodeApp/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  DesignCodeApp
+//
+//  Created by Malcolm Kumwenda on 2017/12/21.
+//  Copyright © 2017 mdevsa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ DesignCodeApp/Source/ViewController/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  DesignCodeApp
+//
+//  Created by Malcolm Kumwenda on 2017/12/21.
+//  Copyright © 2017 mdevsa. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red 
+    }
+}
